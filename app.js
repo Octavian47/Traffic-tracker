e52f2764537da2328b0aeb06f7c19df9a03b2703
Tavitu97 <109802262+Octavian47@users.noreply.github.com>
2023-05-03 21:03:06 +0300

    /stats endpoint and a simple dashboard for testing
    Created the /stats endpoint, where I retrieve information from the database between a specific time frame. At the moment, the data is displayed in a simple html table. No data visualization integrated yet

--- app.js
+++ app.js
@@ -5,21 +5,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Require and include the tracking module
+// Serve static files from the public folder
+app.use(express.static('public'));
+
+// Require and include the tracking and stats modules
 const tracking = require('./track');
+const stats = require('./stats'); // Assuming you have saved the provided stats code as "stats.js"
 
-// Add the tracking module to the app instance
-tracking(app); 
+// Add the tracking and stats modules to the app instance
+tracking(app);
+stats(app);
 
-
-//Test the app
+// Test the app
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 // Start the server listening on the specified port
 const server = app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
-  
+  console.log(`Server running at http://localhost:${port}`);
+});
+
 module.exports = server;
